74c28b0be9d533107fb890cee91b1f13ec38b30a
Isaac Jung <ijung01@mica.edu>
2021-04-16 01:32:35 +0900

    Code Updated
    added csv file for comparing data

--- 3.mapping-quantities/examples/2.table-from-csv/sketch.js
+++ 3.mapping-quantities/examples/2.table-from-csv/sketch.js
@@ -6,14 +6,18 @@ function preload(){
   totals = loadTable('data/totals.csv', 'csv', 'header')
   atmospheric = loadTable('data/atmospheric.csv', 'csv', 'header')
   underground = loadTable('data/underground.csv', 'csv', 'header')
+
+  comparison = loadTable('data/comparison.csv', 'csv', 'header')
+  summits = loadTable('data/summits.csv', 'csv', 'header')
 }
 
 function setup(){
-  createCanvas(3200, 600)
+  createCanvas(2800, 600)
   background(230)
 
   // pick one of the three data files to work with and call it 'table'
-  var table = totals
+  var table = summits
+  var tablelayered = comparison 
 
   // log the whole dataset to the console so we can poke around in it
   print(table)
@@ -39,7 +43,7 @@ function setup(){
 
 
   // draw year labels in the header row
-  x = 200
+  x = 214
   y = 100
   textStyle(NORMAL)
   textAlign(BOLD)
@@ -49,16 +53,38 @@ function setup(){
     x += colWidth
   }
 
+  fill('rgba(0, 255, 20,.25)')
+
   // print out the total for each country, one column at a time
   x = 200
   for (var r=0; r<table.getRowCount(); r++){
     y = 100
     for (var c=1; c<table.getColumnCount(); c++){
       var value = table.getNum(r, c)
-      text(value, x, y)
+      // text(value, x, y)
+      circle(x, y-4, value*18)
       y += rowHeight
     }
     x += colWidth
   }
+
+  // var hrLocation = map(now.hour, 0, 23, 0, hrHeight)
+
+  fill('rgba(255, 50, 0,.25)')
+
+  x = 200
+  for (var r=0; r<tablelayered.getRowCount(); r++){
+    y = 100
+    for (var c=1; c<tablelayered.getColumnCount(); c++){
+      var value = tablelayered.getNum(r, c)
+      // text(value, x, y)
+      circle(x, y-4, value)
+      y += rowHeight
+    }
+    x += colWidth
+  }
+
+
+
 
 }
